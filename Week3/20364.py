45945af35be086eeace15044c9d8f3a3c86083cc
kimmingyu <soil0201@gmail.com>
2022-03-05 02:23:43 +0900

    Week 3 20364

--- Week3/20364.py
+++ Week3/20364.py
@@ -5,40 +5,27 @@ si = sys.stdin.readline
 
 n, q = map(int, si().split())
 
-wanted = [-1] * q
+wanted = [int(si()) for _ in range(q)]
 current = [False] * (n + 1)
-for duck in range(q):
-    wanted[duck] = int(si())
 
 
 def solution(arr):
     # 목적지리스트 순회
     for dest in arr:
-        # temp에 목적지 저장
         temp = dest
-        # flag 설정 -->
-        flag = True
-        # temp가 0이 될 때 까지 2로 나누어준다. --> 경로를 알 수 있음
-        while temp > 0:
+        if current[temp]:
+            result = temp
+        else:
+            result = 0
+        # dest가 1이 될 때 까지 2로 나누어준다. --> 경로를 알 수 있음
+        while dest > 1:
             # 만약 2로 나눈 몫의 current가 참이면 이미 구매된 땅.
-            if current[temp // 2]:
-                # flag를 줘서 도착못했음을 알린다.
-                flag = False
-                # print 해준다.
-                yield temp // 2
-                break
-            else:
-                # 리프에서 현재까지는 구매된 땅이 없었으므로 계속 진행
-                temp //= 2
-
-        # 경로에는 구매된 땅이 없으나, 목적지가 구매된 경우
-        if flag and current[dest]:
-            yield dest
-        # 경로에도 구매된 땅이 없었고ㅡ 목적지도 구매되지 않았다.
-        elif flag:
-            # true로 만들어주고 0 print
-            current[dest] = True
-            yield 0
+            if current[dest // 2]:
+                # result를 갱신시켜준다. --> 경로 중 가장 1에 가까운 곳으로 계속 갱신
+                result = dest // 2
+            dest //= 2
+        current[temp] = True
+        yield result
 
 
 for ans in solution(wanted):

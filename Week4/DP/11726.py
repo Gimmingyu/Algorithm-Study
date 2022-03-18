1c7011f9a8d28b2602915f3a7a2cdc873f992b07
kimmingyu <soil0201@gmail.com>
2022-03-18 18:56:09 +0900

    Week4 11726 Edit table. 2579 Fin.

--- Week4/DP/11726.py
+++ Week4/DP/11726.py
@@ -2,11 +2,11 @@ import sys
 
 si = sys.stdin.readline
 n = int(si())
-table = [0] * (n + 1)
+table = [0] * 1001
 
 table[1] = 1
 table[2] = 2
-for i in range(3, n + 1):
-    table[i] = table[i - 1] + table[i - 2]
+for i in range(3, 1001):
+    table[i] = table[i - 2] + table[i - 1]
 
 print(table[n] % 10007)
